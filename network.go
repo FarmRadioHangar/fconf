1bc23c8540ff1ec9b2c51ba858666fe930131e58
gernest <geofreyernest@live.com>
2016-12-14 09:15:03 +0300

    rename Ethernet to Network

---
+++ network.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/coreos/go-systemd/unit"
+)
+
+//Static holds information for static IP address.
+type Static struct {
+
+	// This is a string representation of an IP address.
+	//
+	// The IP adress should also contain the mask, in the format
+	//xxx.xxx.xxx.xxx/xx
+	IP      string `json:"ip"`
+	Gateway string `json:"gateway"`
+}
+
+//Network configuration settings
+type Network struct {
+	Static    *Static  `json:"static"`
+	DHCP      bool     `json:"dhcp"`
+	DNS       []string `json:"dns-servers"`
+	Interface string   `json:"interface"`
+}
+
+//ToSystemdUnit transforms the Network object to systemd unit file.
+func (e *Network) ToSystemdUnit() ([]*unit.UnitOption, error) {
+	var result []*unit.UnitOption
+	// network interface
+	i := &unit.UnitOption{
+		Section: "Match",
+		Name:    "Name",
+		Value:   "eth0",
+	}
+	if e.Interface != "" {
+		i.Value = e.Interface
+	}
+	result = append(result, i)
+	if e.Static != nil {
+		// add the IP
+		result = append(result, &unit.UnitOption{
+			Section: "Network",
+			Name:    "Address",
+			Value:   e.Static.IP,
+		})
+
+		if !e.DHCP {
+			// Gateway
+			result = append(result, &unit.UnitOption{
+				Section: "Network",
+				Name:    "Gateway",
+				Value:   e.Static.Gateway,
+			})
+		} else {
+			result = append(result, &unit.UnitOption{
+				Section: "Network",
+				Name:    "DHCP",
+				Value:   "ipv4",
+			})
+		}
+		// DNS
+		for _, v := range e.DNS {
+			result = append(result, &unit.UnitOption{
+				Section: "Network",
+				Name:    "DNS",
+				Value:   v,
+			})
+		}
+		return result, nil
+	}
+
+	if e.DHCP {
+		result = append(result, &unit.UnitOption{
+			Section: "Network",
+			Name:    "DHCP",
+			Value:   "ipv4",
+		})
+		if e.DNS != nil {
+			for _, v := range e.DNS {
+				result = append(result, &unit.UnitOption{
+					Section: "Network",
+					Name:    "DNS",
+					Value:   v,
+				})
+			}
+		}
+		return result, nil
+	}
+
+	return nil, errors.New("at least either static should specifid or dhcp")
+}
---
+++ network_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/coreos/go-systemd/unit"
+)
+
+func TestNetwork_ToSystemdUnit(t *testing.T) {
+	e := &Network{
+		Static: &Static{
+			IP:      "192.168.1.8/24",
+			Gateway: "192.168.1.1",
+		},
+		DNS:       []string{"8.8.8.8", "8.8.8.8"},
+		Interface: "eth0",
+	}
+	u, err := e.ToSystemdUnit()
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := unit.Serialize(u)
+	o, _ := ioutil.ReadAll(r)
+	exp, err := ioutil.ReadFile("fixture/staticonly.service")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(exp, o) {
+		t.Errorf("expected \n %s \n Got \n %s", string(exp), string(o))
+	}
+
+	// static and dhcp
+	e.DHCP = true
+	u, err = e.ToSystemdUnit()
+	if err != nil {
+		t.Fatal(err)
+	}
+	r = unit.Serialize(u)
+	o, _ = ioutil.ReadAll(r)
+	exp, err = ioutil.ReadFile("fixture/staticanddhcp.service")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(exp, o) {
+		t.Errorf("expected \n %s \n Got \n %s", string(exp), string(o))
+	}
+
+	// static only
+	e.Static = nil
+	u, err = e.ToSystemdUnit()
+	if err != nil {
+		t.Fatal(err)
+	}
+	r = unit.Serialize(u)
+	o, _ = ioutil.ReadAll(r)
+	exp, err = ioutil.ReadFile("fixture/dhcponly.service")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(exp, o) {
+		t.Errorf("expected \n %s \n Got \n %s", string(exp), string(o))
+	}
+
+}
